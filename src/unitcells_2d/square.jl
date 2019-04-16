818b96f37b0bb90f163812561e562b5bc7ac4ed0
Martin Gembe <mgembe@l25.thp.uni-koeln.de>
2019-04-16 13:07:11 +0200

    added implementation of the square lattice unitcell with diagonal bonds for the J1-J2-model

--- src/unitcells_2d/square.jl
+++ src/unitcells_2d/square.jl
@@ -38,7 +38,41 @@ function getUnitcellSquare(
 end
 
 
+# Implementation
+# - implementation 2 for the J1-J2 model
+# - labels <: Any
+function getUnitcellSquare(
+            unitcell_type  :: Type{U},
+            implementation :: Val{2}
+        ) :: U where {LS,LB,S<:AbstractSite{LS,2},B<:AbstractBond{LB,2},U<:AbstractUnitcell{S,B}}
 
+    # return a new Unitcell
+    return newUnitcell(
+        # Type of the unitcell
+        U,
+        # lattice vectors
+        Vector{Float64}[
+            Float64[1, 0],
+            Float64[0, 1]
+        ],
+        # sites
+        S[
+            newSite(S, Float64[0,0], getDefaultLabelN(LS,1))
+        ],
+        # bonds
+        B[
+            newBond(B, 1,1, getDefaultLabelX(LB), (+1,0)),
+            newBond(B, 1,1, getDefaultLabelX(LB), (-1,0)),
+            newBond(B, 1,1, getDefaultLabelX(LB), (0,+1)),
+            newBond(B, 1,1, getDefaultLabelX(LB), (0,-1)),
+
+            newBond(B, 1,1, getDefaultLabelY(LB), (+1,+1)),
+            newBond(B, 1,1, getDefaultLabelY(LB), (+1,-1)),
+            newBond(B, 1,1, getDefaultLabelY(LB), (-1,+1)),
+            newBond(B, 1,1, getDefaultLabelY(LB), (-1,-1))
+        ]
+    )
+end
 
 
 # Implementation
